d972c981d9f1f53d04b53bfda47f1ec3985f220f
Pavan-1410 <pavansonawane004@gmail.com>
2026-03-04 21:36:21 +0530

    new code

--- aruch.py
+++ aruch.py
@@ -5,3 +5,13 @@ def give_me_rs():
 fruits = ["apple", "banana", "cherry"]
 for fruit in fruits:
   print(fruit)
+
+# Assign values to variables
+num1 = 50
+num2 = 270
+
+# Add the numbers
+sum = num1 + num2
+
+# Print the result using an f-string
+print(f"The sum of 2 numbers is: {sum}")
